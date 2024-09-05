a0aa47e086a4747df822c977473106070ad998c2
Rajrup-TransEV <transmogrify17@outlook.com>
2024-09-05 16:18:54 +0530

    some more improvements added

--- src/pages/addcharger.jsx
+++ src/pages/addcharger.jsx
@@ -381,7 +381,7 @@ return (
             <button
               className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
             >
-                          {loading ? 'Loading...' : 'Save charger details'}
+                          {loading ? 'Processing...' : 'Save charger details'}
             </button>
           </div>
         </form>
